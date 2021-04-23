34ea4dcd4b5816c3b55d2f7e4c69b7965d6ecbb6
unknown <akshai10kb@gmail.com>
2021-04-23 16:50:45 +0530

    first commit

---
+++ namepara.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System;
+namespace Day_2
+{
+    class Class14
+    {
+        public static void addstring(string s1,string s2,string s3)
+        {
+            Console.WriteLine(s1+s2+s3);
+            
+        }
+        static void Main()
+        {
+            addstring(s1:"Akki",s2:" ",s3:"Nutty");
+            Console.ReadLine();
+        }
+    }
+}
